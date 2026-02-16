59677fc90d3fa12064ecb3b5e0728302c146d31d
Willians <williansvpf@gmail.com>
2026-02-16 11:05:43 -0300

    criado propriedades de navegação em ApplicationUser

--- App/TaskHub.Domain/Entities/ApplicationUser.cs
+++ App/TaskHub.Domain/Entities/ApplicationUser.cs
@@ -8,4 +8,9 @@ public class ApplicationUser : IdentityUser
     public string Sobrenome { get; set; } = string.Empty;
     public bool Ativo { get; set; } = true;
     public DateTime DataCadastro { get; set; } = DateTime.Now;
+
+    public IEnumerable<Responsavel> Responsaveis { get; set; } = null!;   
+    public IEnumerable<Tarefa> Tarefas { get; set; } = null!;
+    public IEnumerable<Projeto> Projetos { get; set; } = null!;
+    public IEnumerable<MembroProjeto> MembroProjetos { get; set; } = null!;
 }
