70f6ecd76b8ee0bc8173abc99f1bcb2adc10f816
WilliansVPF <williansvpf@gmail.com>
2026-04-11 20:40:41 -0300

    acerto na CadatrarTarefaDTO para melhor exibição dos exemplos no swagger

--- App/TaskHub.Application/DTOs/Tarefa/CadastrarTarefaDTO.cs
+++ App/TaskHub.Application/DTOs/Tarefa/CadastrarTarefaDTO.cs
@@ -1,4 +1,5 @@
-using TaskHub.Domain.Enums;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace TaskHub.Application.DTOs.Tarefa;
 
@@ -6,8 +7,14 @@ public class CadastrarTarefaDTO
 {
     public string Titulo { get; set; }
     public string Descricao { get; set; }
+
+    [DefaultValue(false)]
     public bool InicioImediato { get; set; }
+
+    [DataType(DataType.Date)]
     public DateTime? DataFim { get; set; }
+    
+    [DefaultValue("null")]
     public int? IdProjeto { get; set; }
 
     public CadastrarTarefaDTO(string titulo, string descricao, DateTime? dataFim, int? idProjeto, bool inicioImediato = false)
